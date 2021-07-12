205e84ac10a4486e8cc7cd65034365c1c44cbfe6
Your Name <you@example.com>
2021-07-12 09:45:36 -0500

    Creacion de beans a cada entidad, y cad uno de sus atributos, creacion del modelo mvc y inclusion de bootstrap offline

--- src/main/java/com/mx/cmc/CMCSystem/controller/ServletUser.java
+++ src/main/java/com/mx/cmc/CMCSystem/controller/ServletUser.java
@@ -1,5 +1,7 @@
 package com.mx.cmc.CMCSystem.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -7,6 +9,7 @@ import java.io.IOException;
 
 @WebServlet(name = "ServletUser", value = "/ServletUser")
 public class ServletUser extends HttpServlet {
+    Logger logger = LoggerFactory.getLogger(ServletUser.class);
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
